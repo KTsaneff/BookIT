193cbc0b869019e0a0982ae2b4712fb560ffee99
krassytsaneff@gmail.com <jealousy01>
2022-12-08 15:21:26 +0200

    Database successfully launched

---
+++ BookIT.Infrastructure/Entities/Company.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookIT.Infrastructure.Entities
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        [MaxLength(30)]
+        public string VATnumber { get; set; } = null!;
+
+        [Required]
+        [MaxLength(80)]
+        public string AccountablePersonFullName { get; set; } = null!;
+
+        [Required]
+        public string HostId { get; set; } = null!;
+
+        [ForeignKey(nameof(HostId))]
+        public Host Host { get; set; } = null!;
+    }
+}
---
+++ BookIT.Infrastructure/Entities/Host.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookIT.Infrastructure.Entities
+{
+    public class Host : IdentityUser
+    {
+        [Required]
+        [StringLength(30)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [StringLength(40)]
+        public string LastName { get; set; } = null!;
+    }
+}
