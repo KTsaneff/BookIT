4406e6b848fe19357858d17139069d18120b4f22
krassytsaneff@gmail.com <jealousy01>
2022-12-10 09:03:53 +0200

    Creating ModelViews

---
+++ BookIT.Infrastructure/Migrations/20221209161429_ApplicationUserEdited.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookIT.Infrastructure.Migrations
+{
+    public partial class ApplicationUserEdited : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
